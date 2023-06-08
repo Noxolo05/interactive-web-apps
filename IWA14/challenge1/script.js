ca4b6185e6a7327125d1552e09711f04bfbb636f
Noxolo05 <n.makhanya@lab.clicklearning.org>
2023-06-08 18:02:21 +0200

    commit

---
+++ IWA14/challenge1/script.js
@@ -0,0 +1,16 @@
+let firstName = 'John';
+let age = 35;
+let hobby1 = 'Coding';
+
+const logTwice = () => {
+  console.log(parameter)
+  console.log(parameter)
+}
+
+function hobby () {
+  logTwice
+  console.log(`Hello, ${firstName} (${age}). I love ${hobby1}!`)
+}
+
+hobby()
+hobby()
